190d9d2650962e0b87be64be07e040f768c8b7f5
Ganidhu Abeysirigoonawardena <ganidhu.abey@gmail.com>
2024-01-22 21:33:31 -0700

    Fixed assert issues.

--- inc/logger.hpp
+++ inc/logger.hpp
@@ -4,6 +4,9 @@
 #include <fmt/core.h>
 
 #ifndef NDEBUG
+
+#include <cassert>
+
 #define WARN(...)                                                        \
   fmt::print(fg(fmt::color::yellow), "[WARNING ( {}() )] - ", __func__); \
   fmt::print(__VA_ARGS__);                                               \
@@ -11,14 +14,14 @@
 #define ERR(...)                                                    \
   fmt::print(stderr, fg(fmt::color::crimson) | fmt::emphasis::bold, \
              "[ERROR ( {}() )] - ", __func__);                      \
-  fmt::print(__VA_ARGS__);                                          \
+  fmt::print(stderr, fg(fmt::color::crimson) | fmt::emphasis::bold, __VA_ARGS__);                                          \
   fmt::print("\n");                                                 \
   assert(false)
 
 #define ERR_LOG(...)                                                \
   fmt::print(stderr, fg(fmt::color::crimson) | fmt::emphasis::bold, \
              "[ERROR ( {}() )] - ", __func__);                      \
-  fmt::print(__VA_ARGS__);                                          \
+  fmt::print(stderr, fg(fmt::color::crimson) | fmt::emphasis::bold, __VA_ARGS__);                                          \
   fmt::print("\n")
 
 #define INFO(...)                             \
